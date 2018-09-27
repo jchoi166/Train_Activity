ca3c2a30d1d6abd79b9ad0dc49b2d8a819cd9d0d
Spike <jchoi166@gmail.com>
2018-09-26 17:56:58 -0700

    functioning code (messy)

--- logic.js
+++ logic.js
@@ -20,12 +20,12 @@ $("#add-info-btn").on("click", function(event){
 
     var trainName = $("#train-name-input").val().trim()
     var destination = $("#destination-name-input").val().trim()
-    var firstTrain = $("#train-time-input").val().trim()
+    var firstTrain = moment($("#train-time-input").val().trim(),"hh:mm").format("hh:mm")
     var frequency = $("#train-frequency-input").val().trim()
 
     // console.log (trainName)
     // console.log (destination)
-    // console.log (firstTrain)
+    console.log (firstTrain)
     // console.log (frequency)
 
     var newTrain = {
@@ -52,23 +52,46 @@ $("#add-info-btn").on("click", function(event){
 });
 
 trainList.on("child_added", function(snapshot){
-    console.log(snapshot.val())
+    // console.log(snapshot.val())
 
     var name = snapshot.val().name
     var trainDestination = snapshot.val().trainDestination
     var firstLeave = snapshot.val().firstLeave
-    var trainFrequency = snapshot.val().trainFrequency
+    var displayFrequency = snapshot.val().trainFrequency
 
-    console.log(name)
-    console.log(trainDestination)
-    console.log(firstLeave)
+    
+    var currentTime = moment().format("hh:mm")
+    console.log("current time" + currentTime)
+
+    var firstTime = moment(firstLeave,"hh:mm")
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log("first time converted:" + firstTimeConverted);
+
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    var trainFrequency = parseInt(displayFrequency)
     console.log(trainFrequency)
+
+    var tRemainder = diffTime % trainFrequency;
+    console.log("tRemainder:" + tRemainder);
+
+    var minutesAway = trainFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + minutesAway);
+
+    var nextTrain = moment().add(minutesAway, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    // console.log(name)
+    // console.log(trainDestination)
+    // console.log(firstLeave)
+    // console.log(trainFrequency)
 
     var newRow = $("<tr>").append(
         $("<td>").text(name),
         $("<td>").text(trainDestination),
-        $("<td>").text(firstLeave),
-        $("<td>").text(trainFrequency)
+        $("<td>").text(displayFrequency),
+        $("<td>").text(moment(nextTrain).format("hh:mm")),
+        $("<td>").text(minutesAway),
     )
 
     $("#train-table > tbody").append(newRow)
